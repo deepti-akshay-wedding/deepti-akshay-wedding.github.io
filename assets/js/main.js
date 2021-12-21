f3021ae37704024d54c17760bdd59663f9d72381
Akshay Narayan <akshayn@mit.edu>
2021-12-21 17:34:10 -0500

    disable various unnecessary things

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,86 +21,6 @@
 		// window.addEventListener
 			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
 
-	// Play initial animations on page load.
-		window.addEventListener('load', function() {
-			window.setTimeout(function() {
-				$body.classList.remove('is-preload');
-			}, 100);
-		});
-
-	// Slideshow Background.
-		(function() {
-
-			// Settings.
-				var settings = {
-
-					// Images (in the format of 'url': 'alignment').
-						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
-						},
-
-					// Delay.
-						delay: 6000
-
-				};
-
-			// Vars.
-				var	pos = 0, lastPos = 0,
-					$wrapper, $bgs = [], $bg,
-					k, v;
-
-			// Create BG wrapper, BGs.
-				$wrapper = document.createElement('div');
-					$wrapper.id = 'bg';
-					$body.appendChild($wrapper);
-
-				for (k in settings.images) {
-
-					// Create BG.
-						$bg = document.createElement('div');
-							$bg.style.backgroundImage = 'url("' + k + '")';
-							$bg.style.backgroundPosition = settings.images[k];
-							$wrapper.appendChild($bg);
-
-					// Add it to array.
-						$bgs.push($bg);
-
-				}
-
-			// Main loop.
-				$bgs[pos].classList.add('visible');
-				$bgs[pos].classList.add('top');
-
-				// Bail if we only have a single BG or the client doesn't support transitions.
-					if ($bgs.length == 1
-					||	!canUse('transition'))
-						return;
-
-				window.setInterval(function() {
-
-					lastPos = pos;
-					pos++;
-
-					// Wrap to beginning if necessary.
-						if (pos >= $bgs.length)
-							pos = 0;
-
-					// Swap top images.
-						$bgs[lastPos].classList.remove('top');
-						$bgs[pos].classList.add('visible');
-						$bgs[pos].classList.add('top');
-
-					// Hide last image after a short delay.
-						window.setTimeout(function() {
-							$bgs[lastPos].classList.remove('visible');
-						}, settings.delay / 2);
-
-				}, settings.delay);
-
-		})();
-
 	// Signup Form.
 		(function() {
 
@@ -136,35 +56,35 @@
 
 			// Events.
 			// Note: If you're *not* using AJAX, get rid of this event listener.
-				$form.addEventListener('submit', function(event) {
+				//$form.addEventListener('submit', function(event) {
 
-					event.stopPropagation();
-					event.preventDefault();
+				//	event.stopPropagation();
+				//	event.preventDefault();
 
-					// Hide message.
-						$message._hide();
+				//	// Hide message.
+				//		$message._hide();
 
-					// Disable submit.
-						$submit.disabled = true;
+				//	// Disable submit.
+				//		//$submit.disabled = true;
 
-					// Process form.
-					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-					// but there's enough here to piece together a working AJAX submission call that does.
-						window.setTimeout(function() {
+				//	// Process form.
+				//	// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+				//	// but there's enough here to piece together a working AJAX submission call that does.
+				//		window.setTimeout(function() {
 
-							// Reset form.
-								$form.reset();
+				//			// Reset form.
+				//				$form.reset();
 
-							// Enable submit.
-								$submit.disabled = false;
+				//			// Enable submit.
+				//				//$submit.disabled = false;
 
-							// Show message.
-								$message._show('success', 'Thank you!');
-								//$message._show('failure', 'Something went wrong. Please try again.');
+				//			// Show message.
+				//				$message._show('success', 'Thank you!');
+				//				//$message._show('failure', 'Something went wrong. Please try again.');
 
-						}, 750);
+				//		}, 750);
 
-				});
+				//});
 
 		})();
 
